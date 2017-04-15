8d3c4ab364e6fc7816a5146f2a1f76e2853f0a15
timeyyy <timeyyy_da_man@hotmail.com>
2017-04-15 18:48:55 +0200

    setup.py install_requires and version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
 	# Versions should comply with PEP440. For a discussion on single-sourcing
 	# the version across setup.py and the project code, see
 	# https://packaging.python.org/en/latest/single_source_version.html
-	version='0.1.1',
+	version='0.1.2',
 	
 	description = 'Collection of tools for building Gui Desktop Applications with tkinter',
 	long_description =long_description,
@@ -60,7 +60,7 @@ setup(
 	# project is installed. For an analysis of "install_requires" vs pip's
 	# requirements files see:
 	# https://packaging.python.org/en/latest/requirements.html
-	#~ install_requires=['peppercorn'],
+	install_requires=['Pillow'],
 	
 	# List additional groups of dependencies here (e.g. development dependencies).
 	# You can install these using the following syntax, for example:
